782786a361a0130c0059832d06267d5ad24383c9
qcollinscode <ilovelife4lyfe@yahoo.com>
2017-02-19 20:54:08 -0500

    updated program

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Project-Javascript-Todo-List
 
 ## To open the console press the `Ctrl` `Shift` `K` ( `Command` `Option` `K` on OS X) keyboard shortcut.
+
+### Version 2
+1. New `delete` option.
--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 var todos = [];
 var input = prompt("What would you like to do?");
-console.log("Enter one of the listed commands.");
+
 
 while(input !== "quit") {
   if(input === "list") {
-    for(var x = 0; x < todos.length; x++) {
-      console.log([x + 1] + ". " + todos[x]);
-    }
+    listTodos();
   } else if(input === "new") {
-    var newTodo = prompt("Enter new todo.");
-    todos.push(newTodo);
+    newTodo();
+  } else if(input === "delete") {
+    deleteTodo();
   }
   input = prompt("What would you like to do?");
+}
+
+function listTodos() {
+  console.log("***************");
+  for(var x = 0; x < todos.length; x++) {
+    console.log([x + 1] + ". " + todos[x]);
+  }
+  console.log("***************");
+}
+
+function newTodo() {
+  var newTodo = prompt("Enter new todo.");
+  todos.push(newTodo);
+  console.log("'" + newTodo + "'" + " : Was added to the list!");
+}
+
+function deleteTodo() {
+  var index = prompt("Enter the number of the todo.");
+  var newIndex = index - 1;
+  todos.splice(newIndex, 1);
+  console.log("Item deleted!");
 }
 
 console.log("OK, Bye!");
