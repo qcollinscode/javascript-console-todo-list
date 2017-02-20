2566007179a5a19c54f83097d59ce3c9329bbf66
qcollinscode <ilovelife4lyfe@yahoo.com>
2017-02-19 19:16:55 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var todos = ["Ok"];
+var input = prompt("What would you like to do?");
+
+
+while(input !== "quit") {
+  if(input === "list") {
+    for(var x = 0; x < todos.length; x++) {
+      console.log([x + 1] + ". " + todos[x]);
+    }
+  } else if(input === "new") {
+    var newTodo = prompt("Enter new todo.");
+    todos.push(newTodo);
+  }
+  input = prompt("What would you like to do?");
+}
+
+console.log("OK, Bye!");
